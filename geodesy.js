ea39fb15437d52824f8490ff97c2cf4eab7eb82e
jazzyjackson <colten.jackson@lookalive.software>
2020-07-11 16:41:06 -0500

    added a Tile interface, now sorting by ArcTan

--- geodesy.js
+++ geodesy.js
@@ -9,48 +9,73 @@ function table(x, y) {
     }
     return grid;
 }
-function array2matrix(outer) {
+function lambdafy(outer) {
+    // wrap brackets around the string elements so Algebrite understands it as a matrix
     return "[" + outer.map(function (inner) { return "[" + inner.join() + "]"; }).join() + "]";
 }
-function matrix2array(matrix) {
-    var num = "\\d";
-    var plus = "\\+";
-    var min = "\\-";
-    var mult = "\\*";
-    var div = "\\/";
-    var pow = "\\^";
-    var comma = "\\,";
-    var parens = "\\(\\)";
-    // this matches number pairs inside the algebrite string
-    // so it returns an array of of array of Algebrite strings
-    return Array.from(matrix.match(new RegExp('[' + num + plus + min + mult + div + pow + parens + ']+' +
-        comma +
-        '[' + num + plus + min + mult + div + pow + parens + ']+', 'g')), function (result) { return result.split(','); });
-}
-function toNumber(λstring) {
+function N(λstring) {
     return λ.float(λstring).d;
 }
-function spin(xypair) {
-    return Math.atan2(toNumber(xypair[0]), toNumber(xypair[1]));
+// if string starts with [[ you've got a matrix to convert!
+// would be better if N worked on single digits, vectors, and matrices, just detected how to walk over it
+// or if I had an apply function that could take N and map it over vector or matrix.
+// but how to return number | number[][], something to avoid?
+// M for matrix, take result from Algebrite and return an array of string values
+function M(matrix) {
+    var comma = "\\,";
+    var math = [
+        "\\d",
+        "\\+",
+        "\\-",
+        "\\*",
+        "\\/",
+        "\\^",
+        "\\(\\)" // parens
+    ].join('');
+    // this matches number pairs inside the algebrite string
+    // so it returns an array of of array of Algebrite strings
+    return Array.from(matrix.match(new RegExp('[' + math + ']+' + comma + '[' + math + ']+', 'g')), function (result) { return result.split(','); });
+}
+function spin(x, y) {
+    return Math.atan2(N(x), N(y));
     // return λ.run(`arctan(${xypair[0]},${xypair[1]})`)
 }
-function norm(xypair) {
-    return λ.run("abs([" + xypair[0] + "," + xypair[1] + "])");
+function norm(x, y) {
+    return λ.run("abs([" + x + "," + y + "])");
 }
 function dot(dimensions, basis) {
-    return λ.run("dot(" + array2matrix(dimensions) + "," + array2matrix(basis) + ")");
+    return λ.run("dot(" + lambdafy(dimensions) + "," + lambdafy(basis) + ")");
 }
-var basispts = matrix2array(dot(table(2, 2), octostars.geodesy[0].basis));
+function applyShift(dimensions, shift) {
+    // I thought of doing this with dot product with a matrix transform but there's a bunch of adding and removing ones to make the shapes fit
+    // so I thought why not just add to x and y
+    var dimensionsClone = dimensions.slice();
+    for (var pt in dimensionsClone) {
+        dimensionsClone[pt][0] = λ.run(dimensionsClone[pt][0] + " + " + shift[0]);
+        dimensionsClone[pt][1] = λ.run(dimensionsClone[pt][1] + " + " + shift[1]);
+    }
+    return dimensionsClone;
+}
+var basispts = M(dot(applyShift(table(4, 4), octostars.geodesy[0].shift), octostars.geodesy[0].basis));
 var norms = {};
 for (var _i = 0, basispts_1 = basispts; _i < basispts_1.length; _i++) {
     var xypair = basispts_1[_i];
-    var thisnorm = norm(xypair);
+    var x = xypair[0], y = xypair[1];
+    var thisnorm = norm(x, y);
+    var thisspin = spin(x, y);
+    var tile = {
+        x: x, y: y,
+        "norm": thisnorm,
+        "spin": thisspin
+    };
     if (norms[thisnorm]) {
-        norms[thisnorm].push([].concat(xypair, spin(xypair)));
+        norms[thisnorm].push(tile);
     }
     else {
-        norms[thisnorm] = [[].concat(xypair, spin(xypair))];
+        norms[thisnorm] = [tile];
     }
 }
-// for every point 
-console.log(norms);
+for (var _a = 0, _b = Object.values(norms); _a < _b.length; _a++) {
+    var el = _b[_a];
+    console.log(el.sort(function (a, b) { return a.spin - b.spin; }));
+}
--- geodesy.ts
+++ geodesy.ts
@@ -11,63 +11,104 @@ function table(x: number ,y: number): string[][] {
     return grid
 }
 
-function array2matrix(outer: string[][]): string {
+function lambdafy(outer: string[][]): string {
+    // wrap brackets around the string elements so Algebrite understands it as a matrix
     return `[${outer.map(inner => `[${inner.join()}]`).join()}]`
 }
 
-function matrix2array(matrix: string): string[][]{
-    let num  = "\\d" 
-    let plus = "\\+"
-    let min  = "\\-"
-    let mult = "\\*"
-    let div  = "\\/"
-    let pow  = "\\^"
-    let comma = "\\,"
-    let parens = "\\(\\)"
-    // this matches number pairs inside the algebrite string
-    // so it returns an array of of array of Algebrite strings
-    return Array.from(matrix.match(new RegExp(
-        '[' + num + plus + min + mult + div + pow + parens + ']+' +
-         comma + 
-        '[' + num + plus + min + mult + div + pow + parens + ']+' ,
-        'g')), result => result.split(','))
-}
-
-function toNumber(λstring: string): number {
+function N(λstring: string): number {
     return λ.float(λstring).d
 }
+// if string starts with [[ you've got a matrix to convert!
+// would be better if N worked on single digits, vectors, and matrices, just detected how to walk over it
+// or if I had an apply function that could take N and map it over vector or matrix.
+// but how to return number | number[][], something to avoid?
 
-function spin(xypair: string[]): number {
-    return Math.atan2(toNumber(xypair[0]), toNumber(xypair[1]))
+// M for matrix, take result from Algebrite and return an array of string values
+
+function M(matrix: string): string[][]{
+    let comma = "\\,"
+    let math = [
+        "\\d",
+        "\\+", // plus
+        "\\-", // sub
+        "\\*", // mult
+        "\\/", // div
+        "\\^", // pow
+        "\\(\\)" // parens
+    ].join('')
+    // this matches number pairs inside the algebrite string
+    // so it returns an array of of array of Algebrite strings
+    return Array.from(
+        matrix.match(
+            new RegExp(
+                '[' + math + ']+' + comma + '[' + math + ']+' ,'g'
+            )
+        ), result => result.split(',')
+    )
+}
+
+
+function spin(x: string, y: string): number {
+    return Math.atan2(N(x), N(y))
     // return λ.run(`arctan(${xypair[0]},${xypair[1]})`)
 }
 
-function norm(xypair: string[]): string {
-    return λ.run(`abs([${xypair[0]},${xypair[1]}])`)
+function norm(x: string, y: string): string {
+    return λ.run(`abs([${x},${y}])`)
 }
 
 function dot(dimensions: string[][], basis: string[][]): string {
-    return λ.run(`dot(${array2matrix(dimensions)},${array2matrix(basis)})`)
+    return λ.run(`dot(${lambdafy(dimensions)},${lambdafy(basis)})`)
 }
 
-let basispts = matrix2array(
+function applyShift(dimensions: string[][], shift: string[]): string[][]{
+    // I thought of doing this with dot product with a matrix transform but there's a bunch of adding and removing ones to make the shapes fit
+    // so I thought why not just add to x and y
+    let dimensionsClone = dimensions.slice()
+    for(var pt in dimensionsClone){
+        dimensionsClone[pt][0] = λ.run(`${dimensionsClone[pt][0]} + ${shift[0]}`)
+        dimensionsClone[pt][1] = λ.run(`${dimensionsClone[pt][1]} + ${shift[1]}`)
+    }
+    return dimensionsClone
+}   
+
+let basispts = M(
     dot(
-        table(2,2),
+        applyShift(table(4,4), octostars.geodesy[0].shift),
         octostars.geodesy[0].basis
     )
 )
 let norms = {}
+// I can push 
+// {norm, spin, polygon#, x, y }
+interface Tile {
+    x: string,
+    y: string,
+    norm: string,
+    spin: number
+}
 
 for(var xypair of basispts){
-    var thisnorm = norm(xypair)
+    var [x,y] = xypair
+    var thisnorm = norm(x,y)
+    var thisspin = spin(x,y)
+
+    var tile:Tile = {
+        x, y,
+        "norm": thisnorm,
+        "spin": thisspin
+    }
+
     if(norms[thisnorm]){
-        norms[thisnorm].push([].concat(xypair, spin(xypair)))
+        norms[thisnorm].push(tile)
     } else {
-        norms[thisnorm] = [[].concat(xypair, spin(xypair))]
+        norms[thisnorm] = [tile]
     }
 }
-// for every point 
-console.log(
-    norms
-)
 
+for(var el of Object.values(norms)){
+    console.log(
+        (el as Tile[]).sort((a,b) => a.spin - b.spin)
+    )
+}
